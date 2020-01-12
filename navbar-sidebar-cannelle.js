d108655355353787463b1250921cb469f88e492e
crazy-hobo <omarshishanischool@gmail.com>
2020-01-11 16:38:27 -0800

    Removed Navbar

--- navbar-sidebar-cannelle.js
+++ navbar-sidebar-cannelle.js
@@ -1,18 +1,5 @@
 const bodyConst = document.querySelector('body');
 const strNavSideBar = String.raw`
-<nav id="navbar-2">
-	<div id="navbar-2-left">
-		<h1 id="h1-cannelle">Cannelle la Demoiselle</h1>
-	</div>
-
-	<a id="navbar-hamburger-a-tag" href="mobile-navigation-page.html">
-		<div id="navbar-hamburger" onclick="hamburgerSwirl(), hamburgerClone(), hamburgerDropdown()"> 
-			<div class="navbar-hamburger-bun"></div>
-			<div class="navbar-hamburger-bun"></div>
-			<div class="navbar-hamburger-bun"></div>
-		</div>
-	</a>
-</nav> <!-- <nav id="navbar-2"> -->
 
 <div id="sidebar-2">
 	<div id="sidebar-links">
