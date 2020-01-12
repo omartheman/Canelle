4f86f1011f6b3e83844f4a690ab10e2d79080a98
crazy-hobo <omarshishanischool@gmail.com>
2020-01-11 16:58:46 -0800

    Added Header font as seperate JS file
    So don't need to add the link on each page, each time we change the font.

---
+++ header-font.js
@@ -0,0 +1,4 @@
+
+//This script is for adding the right header font to each page.
+const headerFont = '<link href="https://fonts.googleapis.com/css?family=Cinzel:700&display=swap" rel="stylesheet">'
+document.querySelector('title').insertAdjacentHTML('beforebegin', headerFont);
