22331ebee3733a623942da683976cd89e91e6c85
crazy-hobo <omarshishanischool@gmail.com>
2020-01-22 12:46:42 -0800

    Add successful javascript to enlarge buttons on hover

--- navbar-sidebar-cannelle.js
+++ navbar-sidebar-cannelle.js
@@ -23,3 +23,21 @@ const strNavSideBar = String.raw`
 `;
 bodyConst.insertAdjacentHTML('afterbegin', strNavSideBar);
 	
+//Create a code using javascript that will make the buttons increase a little bit in size when you hover.
+//Use event listener for hover.
+
+//How to select all of the buttons using a javascript document.
+//Use a loop? 
+const sidebarLinksObj = document.querySelector('#sidebar-links').getElementsByTagName('a');
+//Write a loop? To make it so that for each one of these, they have an event listener that listens for hover.....
+
+for (let i = 0; i < sidebarLinksObj.length; i++) {
+
+	sidebarLinksObj[i].addEventListener('mouseover', function() {
+			this.style.transform = 'scale(1.1)';
+	});
+
+	sidebarLinksObj[i].addEventListener('mouseout', function() {
+			this.style.transform = 'scale(1)';
+	});
+};
