560778709d2b4419e0c2473c37149749b1c27f31
crazy-hobo <omarshishanischool@gmail.com>
2020-02-26 10:03:43 -0800

    Remove hamb clone() dropdown(), old functions

--- navbar-sidebar-cannelle.js
+++ navbar-sidebar-cannelle.js
@@ -1,3 +1,11 @@
+const titleConst = document.querySelector('title');
+titleConst.insertAdjacentHTML('beforebegin', String.raw`
+<script
+src="https://code.jquery.com/jquery-3.4.1.js"
+integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+crossorigin="anonymous">
+</script>
+`);
 const bodyConst = document.querySelector('body');
 const strNavSideBar = String.raw`
 
@@ -32,7 +40,7 @@ bodyConst.insertAdjacentHTML('afterbegin', strNavSideBar);
 //Following code inserts hamburger on each page
 const mainConst = document.querySelector('main');
 const hamburger = String.raw`	
-		<div id="navbar-hamburger" hamburgerClone(), hamburgerDropdown()"> 
+		<div id="navbar-hamburger> 
 			<div class="navbar-hamburger-bun"></div>
 			<div class="navbar-hamburger-bun"></div>
 			<div class="navbar-hamburger-bun"></div>
@@ -42,7 +50,6 @@ mainConst.insertAdjacentHTML('beforeend', hamburger);
 	
 //Following code inserts Google fonts on each page.
 
-const titleConst = document.querySelector('title');
 const googleFonts = String.raw`
 <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
@@ -66,3 +73,32 @@ for (let i = 0; i < sidebarLinksObj.length; i++) {
 $('#navbar-hamburger').on('click', function(){
 	$('#mobile-navigation-main-content').slideToggle();
 });
+
+bodyConst.insertAdjacentHTML('afterend', String.raw`
+
+<div id="mobile-navigation-main-content">
+<nav id="navbar-mobile">
+	<a href="index.html">
+		Welcome & Contact
+	</a>
+	<a href="reading-video.html">
+		Reading Video
+	</a>
+	<a href="ebook-donate.html">
+		eBook
+	</a>
+	<a href="proverbs.html">
+		French Proverbs
+	</a>
+	<a href="links-i-like.html">
+		Links I Like
+	</a>
+	<a href="thanks.html">
+		Thanks
+	</a>
+	<a href="coming-soon.html">
+		Coming Soon
+	</a>
+</nav> <!-- <nav id="navbar-3"> -->
+</div> <!-- <div id="mobile-navigation-main-content"> -->
+`);
